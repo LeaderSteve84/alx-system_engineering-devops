74f5eb9bced290e24e7651b0beb79ecba682c5b4
stephen_adah <mailstephenreal@gmail.com>
2023-12-28 12:41:54 +0100

    corrected

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install version of flask (2.1.0)
 #!/usr/bin/pup
-package {'flask':
-  ensure   => '2.1.0',
+package {['python', 'flask', 'werzeug']:
+  ensure   => '3.8.10', '2.1.0', '2.1.1',
   provider => 'pip3'
 }
