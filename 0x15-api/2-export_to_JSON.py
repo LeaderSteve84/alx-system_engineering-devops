d7288e04b447ba62e313447fe0ad689aac5a0bd1
stephen_adah <mailstephenreal@gmail.com>
2024-01-31 23:02:25 +0100

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+export data in the CSV format..
+"""
+import csv
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+    else:
+        user_id = sys.argv[1]
+        user_url = 'https://jsonplaceholder.typicode.com/users/' + user_id
+        response = requests.get(user_url)
+        user_name = response.json().get('username')
+        task_url = user_url + '/todos'
+        response = requests.get(task_url)
+        tasks = response.json()
+
+        dict_data = {user_id: []}
+        for task in tasks:
+            COMPLETED_TASK_STATUS = task.get('completed')
+            TASK_TITLE =  task.get('title')
+            dict_data[user_id].append({
+                "task": TASK_TITLE,
+                "completed": COMPLETED_TASK_STATUS,
+                "username": user_name})
+
+        with open('{}.csv'.format(user_id), 'w') as f:
+            json.dump(dict_data, f)
