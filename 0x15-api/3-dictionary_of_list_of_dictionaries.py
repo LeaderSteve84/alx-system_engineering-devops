7ca2ce1a1dc0f6ad2652bef56bac3c63f9fced2e
stephen_adah <mailstephenreal@gmail.com>
2024-01-31 23:32:18 +0100

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -25,11 +25,11 @@ if __name__ == "__main__":
         users_dict[user_id] = []
         for task in tasks:
             COMPLETED_TASK_STATUS = task.get('completed')
-            TASK_TITLE =task.get('title')
+            TASK_TITLE = task.get('title')
             users_dict[user_id].append({
                 "task": TASK_TITLE,
                 "completed": COMPLETED_TASK_STATUS,
                 "username": user_name
             })
     with open('todo_all_employees.json', 'w') as f:
-            json.dump(users_dict, f)
+        json.dump(users_dict, f)
