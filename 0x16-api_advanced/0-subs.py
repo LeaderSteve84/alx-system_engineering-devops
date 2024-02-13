7053d6cf027ebe386b79cdf8808f16be7fb6085e
stephen_adah <mailstephenreal@gmail.com>
2024-02-13 21:24:10 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and returns the number of subscribers 
+queries the Reddit API and returns the number of subscribers
 """
 import requests
 from sys import argv
