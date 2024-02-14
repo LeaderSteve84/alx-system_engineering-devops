9eae76ff556060e912657507a107c6c05e662db3
stephen_adah <mailstephenreal@gmail.com>
2024-02-14 10:54:01 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,13 +12,17 @@ def number_of_subscribers(subreddit):
     subscribers (not active users, total subscribers)
     for a given subreddit
     """
-    user = {'User-Agent': 'ketralnis'}
-    url = requests.get(
-            'https://www.reddit.com/r/{}/about.json'.format(
-                subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        'AppleWebKit/537.36'
+        '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
 
 
