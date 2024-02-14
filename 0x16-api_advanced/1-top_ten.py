a1fba2bf84a3459fde36b3fce2d470f45658fad3
stephen_adah <mailstephenreal@gmail.com>
2024-02-14 12:28:19 +0100

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,6 +16,7 @@ def top_ten(subreddit):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
         'AppleWebKit/537.36'
         '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'Edg/91.0.864.59'
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
