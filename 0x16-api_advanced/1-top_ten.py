4cba83ef1cb8974bd3e274672ccedc80ae00d55a
stephen_adah <mailstephenreal@gmail.com>
2024-02-14 11:48:54 +0100

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 """
-queries the Reddit API 
+queries the Reddit API and returns the number of subscribers
 """
 import requests
-from sys import argv
+
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and
-    prints the titles of the first 10 hot posts
-    listed for a given subreddit.
+    queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers)
+    for a given subreddit
     """
-    
-
-if __name__ == "__main__":
-    top_ten(sys.argv[1])
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        'AppleWebKit/537.36'
+        '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
