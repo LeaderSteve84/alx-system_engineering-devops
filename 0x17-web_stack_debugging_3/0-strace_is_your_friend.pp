a9b813670f61841b201d3c3a4b60533299e29362
stephen_adah <mailstephenreal@gmail.com>
2024-02-17 10:04:37 +0100

    correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #  fix why Apache is returning a 500 error
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-setting.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
