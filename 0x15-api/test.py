c224d047c7c68d563e22417e09b088f1c84313d2
stephen_adah <mailstephenreal@gmail.com>
2024-02-18 19:06:50 +0100

    all commits

---
+++ 0x15-api/test.py
@@ -0,0 +1,35 @@
+#!/home/stephen_adah/.pyenv/versions/3.8.12/envs/my_project_env_3.8.12/bin/python3
+import requests
+import sys
+
+def fetch_employee_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    todo_url = f"{base_url}/{employee_id}/todos"
+    response = requests.get(base_url)
+    print(response.content)
+    print("------------------")
+    response = requests.get(todo_url)
+    print(response.content)
+    """try:
+        response = requests.get(todo_url)
+        response.raise_for_status()
+        todos = response.json()
+
+        user_response = requests.get(f"{base_url}/{employee_id}")
+        user = user_response.json()
+        employee_name = user.get("name")
+
+        completed_tasks = [task for task in todos if task["completed"]]
+        total_tasks = len(todos)
+
+        print(f"Employee {employee_name} is done with tasks ({len(completed_tasks)}/{total_tasks}):")
+        print(f"{employee_name}:", len(completed_tasks), total_tasks)
+        for task in completed_tasks:
+            print(f"\t{task['title']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")"""
+
+
+if __name__ == "__main__":
+    fetch_employee_todo_progress(2)
