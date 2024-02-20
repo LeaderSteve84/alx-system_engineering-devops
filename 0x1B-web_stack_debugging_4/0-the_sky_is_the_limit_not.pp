345ed153a8f9aabe4fe5c93e42dd152e1fbf9c0b
stephen_adah <mailstephenreal@gmail.com>
2024-02-20 12:44:12 +0100

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
 # to increases the amount of traffic which nginx can handle
 
-exec { 'file--for-nginx':
-  command => "/bin/sed -i 's/15/4094/' /etc/default/nginx",
-  path    => '/usr/local/bin/:/bin/',
-}
-
-exec { 'nginx-restart':
-  command => '/etc/init.d/nginx restart',
-  path    => '/etc/init.d/',
+exec { 'file-for-nginx':
+  command => "/bin/sed -i 's/15/4094/' /etc/default/nginx && sudo service nginx restart",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/usr/games:/usr/local/games',
 }
